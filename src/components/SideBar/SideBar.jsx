1992d628780b49ae6fc68f4b6a6c66a2b0c4ccb2
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-10-03 21:57:19 +0530

    done with the side bar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,13 +2,17 @@ import React from "react"
 import Select from 'react-select'
 import { useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser ,faCar} from '@fortawesome/free-solid-svg-icons';
+import { faUser ,faCar, faClose, faBars} from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom";
 export default function SideBar(){
     const [user,setUser]=useState('')
     const navigate=useNavigate()
+    const[isHidden,setIsHidden]=useState(false)
    return(
-    <div className="h-auto fixed grid gap-5 w-[20%] top-0 left-0 m-2 rounded-md p-4 bg-gray-200 shadow-lg">
+    <div className={`h-auto fixed grid gap-5 w-[20%] top-0 ${isHidden?'transform -translate-x-full':''} left-0 my-2 rounded-md p-4 bg-gray-200 shadow-lg transition-all duration-200`}>
+        {isHidden?<div className="absolute top-1 left-[100%] cursor-pointer" onClick={()=>setIsHidden(!isHidden)}><FontAwesomeIcon icon={faBars} size="2x"/></div>
+        :<div className="absolute top-1 right-2 cursor-pointer" onClick={()=>setIsHidden(!isHidden)}><FontAwesomeIcon icon={faClose} size="2x" /></div>
+        }
         <div className="flex items-start w-full p-2 gap-10">
             <FontAwesomeIcon icon={faUser} size='2x'/>
             <p>Guest</p>
